e60dd8bbac08f1bb6b52f391c2dfdd4bae17aeab
Anudeepjadi <anudeepjadi.22@gmail.com>
2026-06-16 12:55:02 +0530

    An updation  on logo animation of splach screen

--- lib/screens/dashboard/tabs/my_info_tab.dart
+++ lib/screens/dashboard/tabs/my_info_tab.dart
@@ -24,7 +24,7 @@ class MyInfoTab extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             const Text(
-              "Ananya Sharma",
+              "Anudeep jaadi",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xFF1E2875)),
             ),
             const Text(
